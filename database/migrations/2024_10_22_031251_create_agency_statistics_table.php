60160143ab4f8fc63adf2c499d29a9113e010d8c
Huang Wei Lun <ygww407@gmail.com>
2024-10-22 11:16:33 +0800

    建立agency_statistics表格

---
+++ database/migrations/2024_10_22_031251_create_agency_statistics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgencyStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agency_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency_type');
+            $table->integer('total_count');
+            $table->integer('government_officials');
+            $table->integer('simple_appointees');
+            $table->integer('recommended_appointees');
+            $table->integer('commissioned_appointees');
+            $table->decimal('average_age', 4, 2);
+            $table->decimal('average_seniority', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agency_statistics');
+    }
+}
