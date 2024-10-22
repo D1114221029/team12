ededa28760a65d3fc1ac8e06897dd1db6b9b5fe0
Huang Wei Lun <ygww407@gmail.com>
2024-10-22 11:48:47 +0800

    增加comment

--- database/migrations/2024_10_22_031251_create_agency_statistics_table.php
+++ database/migrations/2024_10_22_031251_create_agency_statistics_table.php
@@ -15,14 +15,14 @@ class CreateAgencyStatisticsTable extends Migration
     {
         Schema::create('agency_statistics', function (Blueprint $table) {
             $table->id();
-            $table->string('agency_type');
-            $table->integer('total_count');
-            $table->integer('government_officials');
-            $table->integer('simple_appointees');
-            $table->integer('recommended_appointees');
-            $table->integer('commissioned_appointees');
-            $table->double('average_age', 4, 2);
-            $table->double('average_seniority', 4, 2);
+            $table->string('agency_type')->comment('機關類別');
+            $table->integer('total_count')->comment('總計(人)');
+            $table->integer('government_officials')->comment('政務人員');
+            $table->integer('simple_appointees')->comment('簡任(派)');
+            $table->integer('recommended_appointees')->comment('薦任(派)');
+            $table->integer('commissioned_appointees')->comment('委任(派)');
+            $table->double('average_age', 4, 2)->comment('平均年齡(歲)');
+            $table->double('average_seniority', 4, 2)->comment('平均年資(年)');
             $table->timestamps();
         });
     }
