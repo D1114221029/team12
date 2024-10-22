9be09fa55eede29440be6474ac6d415f3ac762df
d1114221029 <d1114221029@gm.lhu.edu.tw>
2024-10-22 10:37:54 +0800

    資料庫修改

--- resources/views/introl_sdgs.blade.php
+++ resources/views/introl_sdgs.blade.php
@@ -3,12 +3,12 @@
 <head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>永續發展目標 (SDGs)</title>
+    <title>(SDGs)永續發展-性別平等</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
-        <h1>永續發展目標 (SDGs)</h1>
+        <h1>(SDGs)永續發展 性別平等</h1>
         <p>聯合國於2015年通過的全球性目標，共有17項。</p>
     </header>   
     <main>
