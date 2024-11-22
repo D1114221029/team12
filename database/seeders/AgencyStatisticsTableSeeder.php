aed89a4d2099e1930d3caaa48076436197ec86a8
Huang Wei Lun <ygww407@gmail.com>
2024-11-22 15:28:56 +0800

    刪掉多餘註解

--- database/seeders/AgencyStatisticsTableSeeder.php
+++ database/seeders/AgencyStatisticsTableSeeder.php
@@ -14,38 +14,6 @@ class AgencyStatisticsTableSeeder extends Seeder
      */
     public function run()
     {
-        /*
-        行政(E)
-        外交(MFA)
-        警察(P)
-        財稅(MOF)
-        文教(MOE)
-        司法(MOJ)
-        經建(MOEC)
-        交通(MOTC)
-        衛生(MOH)
-        社會(MOW)
-        總計(ALL)
-        男(W)
-        女(F)
-
-        DBmode
-        DB::table(`agency_statistics`)->insert([
-            'agency_type' => '一般行政機關總計',//機關類別
-            'total_count' => 61633,//總計(人)
-            'government_officials'=> 246,//政務人員
-            'simple_appointees'=> 3374,//簡任(派)
-            'recommended_appointees'=> 33647,//薦任(派)
-            'commissioned_appointees'=> 14559,//委任(派)
-            'average_age'=> 43.66,//平均年齡(歲)
-            'average_seniority'=> 15.35,//平均年資(年)
-            'created_at'=>now(),
-            'updated_at'=> now()
-        ]);
-
-
-         */
-
         $ALLagency_type = array('一般行政機關總計','外交僑務機關總計','警察行政機關總計','財稅行政機關總計','文教行政機關總計','司法行政機關總計','經建行政機關總計','交通行政機關總計','衛生行政機關總計','社會福利機關總計');
         $Wagency_type = array('一般行政機關男性','外交僑務機關男性','警察行政機關男性','財稅行政機關男性','文教行政機關男性','司法行政機關男性','經建行政機關男性','交通行政機關男性','衛生行政機關男性','社會福利機關男性');
         $Fagency_type = array('一般行政機關女性','外交僑務機關女性','警察行政機關女性','財稅行政機關女性','文教行政機關女性','司法行政機關女性','經建行政機關女性','交通行政機關女性','衛生行政機關女性','社會福利機關女性');
