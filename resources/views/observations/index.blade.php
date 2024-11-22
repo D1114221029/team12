02dc32cb6362129c8b931c5deb1150fbdcb9026c
Huang Wei Lun <ygww407@gmail.com>
2024-11-22 15:13:35 +0800

    11/22

--- resources/views/observations/index.blade.php
+++ resources/views/observations/index.blade.php
@@ -52,44 +52,40 @@
     </style>
 </head>
 <body>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            @include('observations.header')
-            @yield('observations_contents')
-            @include('observations.footer')
-        </div>
-    </div>
+    <header>
+        全國公務人員數案機關分類
+    </header>
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>編號</th>
+                    <th>機關類別</th>
+                    <th>總計(人)</th>
+                    <th>政務人員</th>
+                    <th>簡任(派)</th>
+                    <th>薦任(派)</th>
+                    <th>委任(派)</th>
+                    <th>平均年齡(歲)</th>
+                    <th>平均年資(年)</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($obervations as $observation)
+                    <tr>
+                        <td>{{$observation->id}}</td>
+                        <td>{{$observation->agency_type}}</td>
+                        <td>{{$observation->total_count}}</td>
+                        <td>{{$observation->government_officials}}</td>
+                        <td>{{$observation->simple_appointees}}</td>
+                        <td>{{$observation->recommended_appointees}}</td>
+                        <td>{{$observation->commissioned_appointees}}</td>
+                        <td>{{$observation->average_age}}</td>
+                        <td>{{$observation->average_seniority}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
 </body>
 </html>
-@section('observations_contents')
-<table>
-    <thead>
-        <tr>
-            <th>編號</th>
-            <th>機關類別</th>
-            <th>總計(人)</th>
-            <th>政務人員</th>
-            <th>簡任(派)</th>
-            <th>薦任(派)</th>
-            <th>委任(派)</th>
-            <th>平均年齡(歲)</th>
-            <th>平均年資(年)</th>
-        </tr>
-    </thead>
-    <tbody>
-            @foreach ($obervations as $observation)
-            <tr>
-                <td>{{$observation->id}}</td>
-                <td>{{$observation->agency_type}}</td>
-                <td>{{$observation->total_count}}</td>
-                <td>{{$observation->government_officials}}</td>
-                <td>{{$observation->simple_appointees}}</td>
-                <td>{{$observation->recommended_appointees}}</td>
-                <td>{{$observation->commissioned_appointees}}</td>
-                <td>{{$observation->average_age}}</td>
-                <td>{{$observation->average_seniority}}</td>
-            </tr>
-            @endforeach
-    </tbody>
-</table>
-@endsection
