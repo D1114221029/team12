e5cfbd1897104da8bbe40bb617227720b931dded
d1114221029 <d1114221029@gm.lhu.edu.tw>
2024-11-26 12:02:04 +0800

    編輯

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,31 @@
 <header>
 <div class="title sdgs">
-G
+<style>
+  div {
+   border: 1px solid gray;
+   padding: 8px;
+  }
+
+  h1 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #4CAF50;
+  }
+
+  p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+  }
+
+  a {
+  text-decoration: none;
+  color: #008CBA;
+  }
+</style>
+<div>
+<h1>
+    全國行政機關公務人員人數
+</h1>
 </div>
 </header>
