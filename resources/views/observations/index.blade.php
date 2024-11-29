2bdeb038e711245698060761e3e1d4d718dd4228
d1114221029 <d1114221029@gm.lhu.edu.tw>
2024-11-29 15:52:57 +0800

    編輯

--- resources/views/observations/index.blade.php
+++ resources/views/observations/index.blade.php
@@ -1,25 +1,49 @@
 @extends('app')
 @section('table')
-    <style>
+<style>
+        #customers 
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        #customers td, #customers th 
+        {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+         #customers th 
+         {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
         body
         {
-          background-color: lightblue;
+            background-color: lightblue;
         }
-    </style>
-        <table>
-                <tr>
-                    <th>機關類型</th>
-                    <th>總人數</th>
-                    <th>政務人員</th>
-                    <th>簡任職人員</th>
-                    <th>薦任職人員</th>
-                    <th>委任職人員</th>
-                    <th>平均年齡</th>
-                    <th>平均年資</th>
-                    <th>操作1</th>
-                    <th>操作2</th>
-                    <th>操作3</th>
-                </tr>
+</style>
+    <table>
+        <tr>
+            <th>機關類型</th>
+            <th>總人數</th>
+            <th>政務人員</th>
+            <th>簡任職人員</th>
+            <th>薦任職人員</th>
+            <th>委任職人員</th>
+            <th>平均年齡</th>
+            <th>平均年資</th>
+            <th>操作1</th>
+            <th>操作2</th>
+            <th>操作3</th>
+        </tr>
             @foreach ($observations as $observation)
                 <tr>
                     <td>{{ $observation->agency_type }}</td>
