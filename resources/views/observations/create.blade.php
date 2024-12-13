30a1ff14a3307ab60323b09c178d0377bce6ed38
Huang Wei Lun <ygww407@gmail.com>
2024-12-13 14:46:17 +0800

    前台數字檢查及防填寫空白

--- resources/views/observations/create.blade.php
+++ resources/views/observations/create.blade.php
@@ -5,35 +5,35 @@
 {!! Form::open(['url'=>'observations/store']) !!}
     <div class="form-group">
         {!! Form::label('agency_type','機關類別')!!}
-        {!! Form::text('agency_type',null,['class'=>'form-control'])!!}
+        {!! Form::text('agency_type',null,['class'=>'form-control','required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('total_count','總計(人)')!!}
-        {!! Form::text('total_count',null,['class'=>'form-control'])!!}
+        {!! Form::number('total_count',null,['class'=>'form-control', 'step' => '1', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('government_officials','政務人員')!!}
-        {!! Form::text('government_officials',null,['class'=>'form-control'])!!}
+        {!! Form::number('government_officials',null,['class'=>'form-control', 'step' => '1', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('simple_appointees','簡任(派)')!!}
-        {!! Form::text('simple_appointees',null,['class'=>'form-control'])!!}
+        {!! Form::number('simple_appointees',null,['class'=>'form-control', 'step' => '1', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('recommended_appointees','薦任(派)')!!}
-        {!! Form::text('recommended_appointees',null,['class'=>'form-control'])!!}
+        {!! Form::number('recommended_appointees',null,['class'=>'form-control', 'step' => '1', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('commissioned_appointees','委任(派)')!!}
-        {!! Form::text('commissioned_appointees',null,['class'=>'form-control'])!!}
+        {!! Form::number('commissioned_appointees',null,['class'=>'form-control', 'step' => '1', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('average_age','平均年齡(歲)')!!}
-        {!! Form::text('average_age',null,['class'=>'form-control'])!!}
+        {!! Form::number('average_age',null,['class'=>'form-control', 'step' => '0.01', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::label('average_seniority','平均年資(年)')!!}
-        {!! Form::text('average_seniority',null,['class'=>'form-control'])!!}
+        {!! Form::number('average_seniority',null,['class'=>'form-control', 'step' => '0.01', 'required' => 'required'])!!}
     </div>
     <div class="form-group">
         {!! Form::submit("新增資料",['class'=>'btn form-control']) !!}
