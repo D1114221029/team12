01eff6d110358c50af83741cb603707378a87f12
Huang Wei Lun <ygww407@gmail.com>
2024-12-31 11:29:55 +0800

    Validation functionality completed

--- app/Http/Controllers/AgencyStatisticsController.php
+++ app/Http/Controllers/AgencyStatisticsController.php
@@ -36,7 +36,7 @@ class AgencyStatisticsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(examine $request)
     {
         $data = $request->validated();
 
